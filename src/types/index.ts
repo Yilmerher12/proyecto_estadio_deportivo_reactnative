1444fd41f271fa1e483f11484327a49a4770c329
Yilmerher12 <yilmerpdf@gmail.com>
2026-08-20 10:44:32 -0500

    feat(week-04): add types for concession items

---
+++ src/types/index.ts
@@ -0,0 +1,13 @@
+// src/types/index.ts
+// Interface principal del dominio.
+// TODO: adaptar a tu dominio asignado.
+
+export interface ItemConcessions {
+    id: string;
+    name: string;
+    description: string;
+    price: number;
+    type: string;
+    stock: number;
+    available: boolean;
+}
